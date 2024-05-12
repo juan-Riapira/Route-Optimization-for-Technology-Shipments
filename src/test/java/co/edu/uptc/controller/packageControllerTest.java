7e16dee0b02ef3925a06e78fbc06808bb6db09bb
Juan-Fernandez <juan.fernandez07@uptc.edu.co>
2024-05-12 15:00:16 -0500

    unit testing correction

---
+++ src/test/java/co/edu/uptc/controller/packageControllerTest.java
@@ -0,0 +1,19 @@
+package co.edu.uptc.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class packageControllerTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(true, true);
+    }
+}
